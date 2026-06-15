db56acaf822e18bf298612acd2eafcdfa85ac0f1
Jeijito <jj.ramgui@gmail.com>
2026-06-15 13:00:36 -0700

    Sixth commit: Command Protocol

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# JEIJITO PRIVATE SERVER
+# JEIJITO PRIVATE SERVER 
 
 This project is a personal project for myself and my family where I am trying to put in practice my knowledge and skills in Networks, Cybersecurity, Data Structures, Git and many other Computer Science topics. :)
+
+# Phase 1
+
+* Part 1
+
+    This is the very first foundation of the server where I create the functions for creating the socket, assigning a port and listen and accept the incoming traffic.
+
+* Part 2
+
+    I added a funtion that translates the raw binary network opcode to be used for instruction assignment. 
--- server.c
+++ server.c
@@ -59,6 +59,39 @@ int main() {
     }
 
     printf("Connection accepted! Client connected via File Descriptor ID: %d\n", client_fd);
+            
+    // Allocate a small 1-byte buffer memory space to read the incoming command opcode
+    unsigned char command_buffer;
+    ssize_t bytes_read;
+
+    printf("Awaiting command byte from client...\n");
+
+    // Read exactly 1 byte from the client communication pipe
+    bytes_read = read(client_fd, &command_buffer, 1);
+
+    if (bytes_read < 0) {
+        perror("Read operation failed");
+    } 
+    else if (bytes_read == 0) {
+        printf("Client disconnected before sending a command.\n");
+    } 
+    else {
+        // Evaluate the command using our protocol definitions
+        switch (command_buffer) {
+            case 0x01:
+                printf("[PROTOCOL SUCCESS] Received Command 0x01: UPLOAD REQUEST triggered.\n");
+                // Future implementation: handle_upload(client_fd);
+                break;
+            case 0x02:
+                printf("[PROTOCOL SUCCESS] Received Command 0x02: DOWNLOAD REQUEST triggered.\n");
+                // Future implementation: handle_download(client_fd);
+                break;
+            default:
+                printf("[PROTOCOL WARNING] Received Unknown Command: 0x%02X. Terminating session.\n", command_buffer);
+                break;
+        }
+    }
+
 
     // 6. Orderly execution teardown (Close client connection, then release the master listener)
     close(client_fd);
